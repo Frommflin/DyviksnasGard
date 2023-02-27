93d8715319f10e1ef4d54c7ea4d32ec6babeec66
Frommflin <freakyfrommelin@outlook.com>
2023-02-27 16:55:56 +0100

    Initializing SPA

---
+++ javascript/general.js
@@ -0,0 +1,10 @@
+function showPage(p) {
+    // Step 1 - Hide all pages
+    pageArr = document.getElementsByClassName("page");
+    for (let i = 0; i < pageArr.length; i++) {
+        let page = pageArr[i];
+        page.style.display = "none";
+    }
+    // Step 2 - Show the selected page
+    document.getElementById("page" + p).style.display = "block";
+}
