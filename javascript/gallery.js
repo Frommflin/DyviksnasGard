40229d44f847282410f4e9675d96916f36af340f
Frommflin <freakyfrommelin@outlook.com>
2023-08-27 12:07:23 +0200

    Adding focus to clicked image
    Adjusting some styles

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -10,3 +10,32 @@ function showAlbum(a) {
     document.getElementById("album" + a).classList.add("activeBtn");
 
 }
+
+function showImage(img){
+    let popup = document.getElementById('galleryPopup');
+    let str = '';
+
+    str += `<div id="popupContent">`;
+    str += `<div><span onclick='closeImage()'>X</span></div>`;
+    str += `<img src="./images/${img}" />`;
+    str += `<p>Kort beskrivning av bilden här</p>`;
+    str += `</div>`;
+
+    popup.style.display='flex';
+    popup.innerHTML = str;
+
+    //Preventing page from scrolling while image is in focus
+    document.body.classList.add('stopScroll');
+    document.body.bind('touchmove', function(e){
+        e.preventDefault()
+    });
+}
+
+function closeImage(){
+    document.getElementById('galleryPopup').style.display='none';
+    document.getElementById('galleryPopup').innerHTML = '';
+
+    //Enabling scrolling on page when image is closed
+    document.body.classList.remove('stopScroll')
+    document.body.unbind('touchmove')
+}
