c3d9fd24526e9a224a0902205da310b6f0d8b048
Frommflin <freakyfrommelin@outlook.com>
2023-09-02 13:43:39 +0200

    C reating form to add image descriptions

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -50,8 +50,34 @@ function showImageForm(){
     str += `<input type="hidden" name="toAlbum" value="albumName" />`;
     str += `<div id="ImgBtnBox">`;
     str += `<button class="btn">Spara</button>`;
-    str += `<button class="btn">Lägg till beskrivningar</button>`;
+    str += `<button class="btn" onclick="addImageDescription()">Lägg till beskrivningar</button>`;
     str += `</div>`;
+    str += `</form>`;
+    str += `</div>`;
+
+    showPopup(str);
+}
+
+function addImageDescription(){
+    let str = '';
+
+    str += `<div class="popupBox">`;
+    str += `<div id="close"><span onclick='closePopup()'>X</span></div>`;
+    str += `<form id="addDescForm" class="col col-xs-6" method="post" enctype="multipart/form-data">`;
+    str += `<div id="hint">Max 100 tecken per bildbeskrivning.</div>`;
+    str += `<div id="cardBox">`;
+    str += `<div class="imageCard">`;
+    str += `<img src="./images/PlaceholderBigImg.png" />`;
+    str += `<div class="input-group">`;
+    str += `<div class="input-group-prepend">`;
+    str += `<span class="input-group-text">Beskrivning</span>`;
+    str += `</div>`;
+    str += `<input type="text" class="form-control" name="imgDescription" maxlength="100" placeholder="Kort beskrivning" required>`;
+    str += `</div>`;
+    str += `</div>`;
+    str += `</div>`;
+    str += `<input type="hidden" name="toAlbum" value="albumName" />`;
+    str += `<button class="btn">Spara</button>`;
     str += `</form>`;
     str += `</div>`;
 
