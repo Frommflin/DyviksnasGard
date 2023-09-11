0d49b46baac9deb4ec4e34f7d16e2ba57607ec18
Frommflin <freakyfrommelin@outlook.com>
2023-09-11 11:56:00 +0200

    Adding password confirmation check

--- javascript/login.js
+++ javascript/login.js
@@ -3,8 +3,27 @@ function changePwd(){
     document.getElementById('forgotPwdBox').style.display='block';
 }
 
-$(document).on('submit', '#forgotPwdForm', function(event){
-    event.preventDefault();
+function showLogin(){
     document.getElementById('loginBox').style.display='flex';
     document.getElementById('forgotPwdBox').style.display='none';
+}
+
+$(document).on('submit', '#forgotPwdForm', function(event){
+    event.preventDefault();
+    showLogin();
 });
+
+function comparePwds(name){
+    let pwd1 = document.getElementById(name + 'Pwd1').value;
+    let pwd2 = document.getElementById(name + 'Pwd2').value;
+    let button = document.getElementById(name + 'Btn');
+    let errorMessage = document.getElementById(name + 'PwdError');
+
+    if(pwd1 === pwd2){
+        button.disabled=false;
+        errorMessage.style.display='none';
+    } else {
+        button.disabled=true;
+        errorMessage.style.display='block';
+    }
+}
