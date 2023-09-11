597c6a20cf852b4d138a09c50f7dfbc26e5075b2
Frommflin <freakyfrommelin@outlook.com>
2023-09-11 09:12:21 +0200

    Adding createUser
    Removing possibility of empty fields

---
+++ php/createUser.php
@@ -0,0 +1,22 @@
+<?php
+    include 'dbConnect.php';
+
+    $email = $_POST('createEmail');
+    $name = $_POST('createName');
+    $pwd = $_POST('createPwd');
+
+    try{
+        $query="INSERT INTO users(email,userName,pwd) values (:EMAIL,:FNAME,:LNAME,:PWD);";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':EMAIL',$email);
+        $stmt->bindParam(':PWD',$pwd);
+        $stmt->execute();
+
+        header ("Content-Type:text/xml; charset=utf-8");  
+        echo '<created status="OK"/>';
+
+    } catch (PDOException $e) {
+            err("Error!: ".$e->getMessage()."<br/>");
+            die();
+    }
+?>
