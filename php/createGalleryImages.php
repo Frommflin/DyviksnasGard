81d2174dec4157b648b5a99e119726bb918be12c
Frommflin <freakyfrommelin@outlook.com>
2023-10-09 16:34:29 +0200

    Create createGalleryImages.php
    Creating backend for saving images to the gallery without image descriptions

---
+++ php/createGalleryImages.php
@@ -0,0 +1,29 @@
+<?php
+    include 'dbConnect.php';
+
+    $album = $_POST['toAlbum'];
+
+    try{
+        foreach($_FILES['galleryUploads']['name'] as $key=>$val){
+            if($_FILES['galleryUploads']['name'] != ''){
+                $extension = explode('.', $_FILES['galleryUploads']['name'][$key]);
+                $newName = rand() . '.' . $extension[1];
+                $destination = '../images/galleryUploads/' . $newName;
+                move_uploaded_file($_FILES['galleryUploads']['tmp_name'][$key], $destination);
+
+                $query='INSERT INTO galleries(img,album) values (:IMG,:ALBUM);';
+                $stmt = $pdo->prepare($query);
+                $stmt->bindParam(':IMG',$newName);
+                $stmt->bindParam(':ALBUM',$album);
+                $stmt->execute();
+
+                header ('Content-Type:text/xml; charset=utf-8');  
+                echo '<created status="OK"/>';
+            }
+        }
+    } 
+    catch (PDOException $e) {
+        err('Error!: '.$e->getMessage().'<br/>');
+        die();
+    }
+?>
