d07f157087721281890760cee45a3720ed9f5ca3
Frommflin <freakyfrommelin@outlook.com>
2023-10-11 13:40:37 +0200

    Creating form upon click
    Moving creation of news-form to function to enable dynamically filled field

---
+++ javascript/news.js
@@ -0,0 +1,33 @@
+
+function showNewsForm(){
+    let str = ``;
+
+    str += `<div class="popupBox">`;
+    str += `<div class="close" onclick="closePopup('news')">&times;</div>`;
+    str += `<form id="addNewsForm" class="col col-xs-6" method="post" enctype="multipart/form-data">`;
+    str += `<h1>Nytt på Dyviksnäs Gård</h1>`;
+    str += `<div class="input-group">`;
+    str += `<div class="input-group-prepend">`;
+    str += `<span class="input-group-text">Titel</span>`;
+    str += `</div>`;
+    str += `<input type="text" class="form-control" name="newsName" maxlength="50">`;
+    str += `</div>`;
+    str += `<div class="input-group">`;
+    str += `<div class="input-group-prepend">`;
+    str += `<span class="input-group-text">Bild</span>`;
+    str += `</div>`;
+    str += `<input type="file" class="form-control" name="newsImg" >`;
+    str += `</div>`;
+    str += `<div class="input-group">`;
+    str += `<div class="input-group-prepend">`;
+    str += `<span class="input-group-text">Meddelande</span>`;
+    str += `</div>`;
+    str += `<textarea class="form-control" name="newsDescription" placeholder="Berätta om nyheten här" required></textarea>`;
+    str += `</div>`;
+    str += `<input type="hidden" name="author" value="USERNAME" />`;
+    str += `<button class="btn">Publicera</button>`;
+    str += `</form>`;
+    str += `</div>`;
+
+    showPopup('news', str);
+}
