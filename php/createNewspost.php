87d4704c6a99aa409c8f4ef03123c60da4723e18
Frommflin <freakyfrommelin@outlook.com>
2023-10-11 15:48:31 +0200

    Update createNewspost.php
    Adding handling for image and empty title-data

--- php/createNewspost.php
+++ php/createNewspost.php
@@ -1,13 +1,21 @@
 <?php
     include 'dbConnect.php';
 
-    $title = $_POST['newsName'];
-    $image = $_FILES['newsImg'];
+    if(isset($_POST['newsName']) ? $title = $_POST['newsName'] : $title = '');
+    $image = '';
     $description = $_POST['newsDescription'];
     $author = $_POST['author'];
     $postDate = date('Y-m-d');
 
     try{
+        if($_FILES['newsImg']['name'] != ''){
+            $extension = explode('.', $_FILES['newsImg']['name']);
+            $newName = rand() . '.' . $extension[1];
+            $destination = '../images/newsUploads/' . $newName;
+            move_uploaded_file($_FILES['newsImg']['tmp_name'], $destination);
+            $image = $newName;
+        }
+
         $query="INSERT INTO newsposts(title,article,author,postDate,img) values (:TITLE,:ARTICLE,:AUTHOR,:POSTDATE,:IMG);";
         $stmt = $pdo->prepare($query);
         $stmt->bindParam(':TITLE',$title);
@@ -21,7 +29,7 @@
         echo '<created status="OK"/>';
 
     } catch (PDOException $e) {
-            err("Error!: ".$e->getMessage()."<br/>");
-            die();
+        err("Error!: ".$e->getMessage()."<br/>");
+        die();
     }
 ?>
