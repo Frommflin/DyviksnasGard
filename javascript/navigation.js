327a883778fc792d8a6d910ff71c6d6310956a98
Frommflin <freakyfrommelin@outlook.com>
2023-11-11 14:59:53 +0100

    Making navbar sticky when scrolling

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -1,3 +1,27 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    // When the user scrolls the page, execute stickyNav
+    window.onscroll = function() {stickyNav()};
+
+    // Get the navbar and content elements
+    var navbar = document.getElementById("navbar");
+    var content = document.getElementById("content");
+
+    // Get the offset position of the navbar
+    var topPos = navbar.offsetTop;
+
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function stickyNav() {
+    if (window.scrollY >= topPos) {
+        navbar.classList.add("stickyNav");
+        content.classList.add("stickyPadding");
+    } else {
+        navbar.classList.remove("stickyNav");
+        content.classList.remove("stickyPadding");
+    }
+    }
+})
+
 function showPage(p, h) {
     // Step 1 - Hide all pages and clear active-tags on navlinks
     pageArr = document.getElementsByClassName("page");
