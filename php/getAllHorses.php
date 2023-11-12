34ed93e71917320465cfa6adfbf0d892386a4ccb
Frommflin <freakyfrommelin@outlook.com>
2023-11-12 11:53:05 +0100

    Create getAllHorses.php

---
+++ php/getAllHorses.php
@@ -0,0 +1,27 @@
+<?php
+    include 'dbConnect.php';
+
+    try
+    {
+        $query = "SELECT id,horseName,img FROM `horses`";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute();
+
+        $output="<horses>\n";
+        foreach($stmt as $key => $row){
+                $output.="<horse \n";
+                $output.="    id='".$row['id']."'\n";
+                $output.="    name='".$row['horseName']."'\n";
+                $output.="    image='".$row['img']."'\n";
+                $output.=" />\n";
+        }
+        $output.="</horses>";
+
+        header ("Content-Type:text/xml; charset=utf-8");
+        echo $output;
+    }
+    catch(PDOException $error)
+    {
+         echo "Error: ".$error->getMessage()."<br/>";
+    }
+?>
