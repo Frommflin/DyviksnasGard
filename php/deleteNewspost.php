75254cc5df8e52f967315f7e5d787f55026434f7
Frommflin <freakyfrommelin@outlook.com>
2023-11-13 17:12:19 +0100

    Create deleteNewspost.php

---
+++ php/deleteNewspost.php
@@ -0,0 +1,20 @@
+<?php
+    include 'dbConnect.php';
+
+    $id = $_POST['postId'];
+
+    try
+    {
+        $query = "DELETE FROM `newsposts` WHERE id=:ID";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':ID',$id);
+        $stmt->execute();
+
+        header ("Content-Type:text/xml; charset=utf-8");
+        echo "<deleted status='OK'/>";
+    }
+    catch(PDOException $error)
+    {
+        echo "Error: ".$error->getMessage()."<br/>";
+    }
+?>
