24ea26992607a2e44585a8e2e00eb23940843f2e
Frommflin <freakyfrommelin@outlook.com>
2023-12-11 16:04:47 +0100

    Adding backend to remove horse from db

---
+++ php/deleteHorseImages.php
@@ -0,0 +1,20 @@
+<?php
+    include "dbConnect.php";
+
+    $id = $_POST["horseId"];
+
+    try
+    {
+        $query = "DELETE FROM `photos` WHERE horse=:ID";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(":ID",$id);
+        $stmt->execute();
+
+        header ("Content-Type:text/xml; charset=utf-8");
+        echo "<deleted status='OK'/>";
+    }
+    catch(PDOException $error)
+    {
+        echo "Error: ".$error->getMessage()."<br/>";
+    }
+?>
