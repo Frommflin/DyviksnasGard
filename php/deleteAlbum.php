4d75db296ab745c495167ebbfbbc1bc80246f9b0
Frommflin <freakyfrommelin@outlook.com>
2023-12-16 13:23:59 +0100

    Adding backend for deleting album and all images in an album

---
+++ php/deleteAlbum.php
@@ -0,0 +1,20 @@
+<?php
+    include "dbConnect.php";
+
+    $id = $_POST["albumId"];
+
+    try
+    {
+        $query = "DELETE FROM `albums` WHERE id=:ID";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(":ID",$id);
+        $stmt->execute();
+
+        header ("Content-Type:text/xml; charset=utf-8");
+        echo "<deleted status='OK'/>";
+    }
+    catch(PDOException $error)
+    {
+        echo "Error: ".$error->getMessage()."<br/>";
+    }
+?>
