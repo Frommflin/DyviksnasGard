52d0f5fc8f8c86186794a91cf4a4b826b41f5958
Frommflin <freakyfrommelin@outlook.com>
2024-07-29 07:07:30 +0200

    Creating form to edit about-section

--- javascript/about.js
+++ javascript/about.js
@@ -1,3 +1,36 @@
+function editAboutForm(id, text, image){
+    let info = makeParagraphs(text, "newLine");
+
+    let str = ``;
+    str += `<form id="updateAbout">`;
+    str += `<textarea class="form-control" name="updateAboutDescription" required>${info}</textarea>`;
+    
+    str += `<div class="input-group">`;
+    str += `<div class="input-group-prepend">`;
+    str += `<span class="input-group-text">Bild</span>`;
+    str += `</div>`;
+    str += `<input type="file" class="form-control" name="newAboutImage">`;
+    str += `</div>`;
+    str += `<span class="hint">`;
+    str += `För att behålla bilden, lämna filuppladdningen tom!`;
+    str += `</span>`;
+    
+    str += `<input type="hidden" name="updateAboutId" value="${id}">`;
+    str += `<input type="hidden" name="oldAboutImage" value="${image}">`;
+
+    str += `<div class="crudBox">`;
+    str += `<button class="btn">`;
+    str += `<img src="./icons/save-white.png" />`;
+    str += `Spara ändring`;
+    str += `</button>`;
+    str += `<button class="btn" onclick="getAbouts()">`;
+    str += `&times Avbryt`;
+    str += `</button>`;
+    str += `</div>`;
+
+    str += `</form>`;
+    document.getElementById(`aboutBox${id}`).innerHTML = str;
+}
 
 // --------------------------------------------------
 // --------------      AJAX CALLS      --------------
@@ -21,8 +54,20 @@ function getAbouts(){
                     str += `<div class="about">`;                    
                     str += `<img src="./images/${about.attributes["img"].nodeValue}" alt="Om ${about.attributes["name"].nodeValue}">`;
                     str += `<div>`;
+                    str += `<div class="topRow">`;
                     str += `<h1>${about.attributes["name"].nodeValue}</h1>`;
+                    if(localStorage.getItem("userRole") === "Admin"){
+                        str += `<div>`;
+                        str += `<button class="crudBtn" onclick="editAboutForm(${about.attributes["id"].nodeValue},'${about.attributes["description"].nodeValue}','${about.attributes["img"].nodeValue}')">`;
+                        str += `<img src="./icons/editpost-white.png" />`;
+                        str += `Redigera kapitel`;
+                        str += `</button>`;
+                        str += `</div>`;
+                    }
+                    str += `</div>`;
+                    str += `<div id="aboutBox${about.attributes["id"].nodeValue}">`;
                     str += info;
+                    str += `</div>`;
                     str += `</div>`;
                     str += `</div>`;
                 }
